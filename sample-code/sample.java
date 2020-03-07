3643cb659ffd587965f6bae46949dc6fd8fe9e8b
Harry Annan <43813256+annanharry@users.noreply.github.com>
2020-03-07 05:07:54 +0300

    Add files via upload

---
+++ sample-code/sample.java
@@ -0,0 +1,13 @@
+class sample{
+	public static void main (String[] args) {
+	int a = 5;
+	int b = 3;
+	int c = 4;
+	int sum;
+	int div;
+	sum = a + b;
+	div = sum / c;
+	System.out.println(sum);
+	System.out.println(div);
+	}
+}
